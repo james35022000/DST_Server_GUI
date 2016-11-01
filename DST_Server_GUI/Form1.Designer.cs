8ae29d4fbe20af469a30e188357b513159116b59
JCChen <james35022000@gmail.com>
2016-11-01 23:27:39 +0800

    DST_Server_GUI

--- DST_Server_GUI/Form1.Designer.cs
+++ DST_Server_GUI/Form1.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox_server = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox_server
+            // 
+            this.comboBox_server.FormattingEnabled = true;
+            this.comboBox_server.Location = new System.Drawing.Point(23, 264);
+            this.comboBox_server.Name = "comboBox_server";
+            this.comboBox_server.Size = new System.Drawing.Size(121, 22);
+            this.comboBox_server.TabIndex = 1;
+            this.comboBox_server.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(331, 304);
+            this.Controls.Add(this.comboBox_server);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "DST Server GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox_server;
     }
 }
 
--- DST_Server_GUI/Form1.cs
+++ DST_Server_GUI/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace DST_Server_GUI
 {
@@ -14,6 +15,24 @@ namespace DST_Server_GUI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox_server.Items.AddRange(new Object[] { "DST_Server1", "DST_Server2" });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "dontstarve_dedicated_server_nullrenderer.exe";
+            info.WorkingDirectory = "C:\\\"Program Files (x86)\"\\Steam\\steamapps\\common\\\"Don't Starve Together Dedicated Server\"\\bin";
+            Process.Start(info);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
