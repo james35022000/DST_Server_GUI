8dd9b56405bcc7816574c065b97c7819df73a89c
JCChen <james35022000@gmail.com>
2016-11-05 00:05:21 +0800

    DST_Server_GUI

--- DST_Server_GUI/Form1.Designer.cs
+++ DST_Server_GUI/Form1.Designer.cs
@@ -31,11 +31,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox_server = new System.Windows.Forms.ComboBox();
             this.Server_output = new System.Windows.Forms.TextBox();
+            this.textBox_Send = new System.Windows.Forms.TextBox();
+            this.button_Send = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(488, 520);
+            this.button1.Location = new System.Drawing.Point(488, 87);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(87, 27);
             this.button1.TabIndex = 0;
@@ -46,7 +48,7 @@
             // comboBox_server
             // 
             this.comboBox_server.FormattingEnabled = true;
-            this.comboBox_server.Location = new System.Drawing.Point(344, 523);
+            this.comboBox_server.Location = new System.Drawing.Point(467, 120);
             this.comboBox_server.Name = "comboBox_server";
             this.comboBox_server.Size = new System.Drawing.Size(121, 22);
             this.comboBox_server.TabIndex = 1;
@@ -54,17 +56,38 @@
             // 
             // Server_output
             // 
-            this.Server_output.Location = new System.Drawing.Point(23, 139);
+            this.Server_output.Location = new System.Drawing.Point(24, 87);
             this.Server_output.Multiline = true;
             this.Server_output.Name = "Server_output";
-            this.Server_output.Size = new System.Drawing.Size(294, 406);
+            this.Server_output.ReadOnly = true;
+            this.Server_output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Server_output.Size = new System.Drawing.Size(435, 406);
             this.Server_output.TabIndex = 2;
+            // 
+            // textBox_Send
+            // 
+            this.textBox_Send.Location = new System.Drawing.Point(24, 520);
+            this.textBox_Send.Name = "textBox_Send";
+            this.textBox_Send.Size = new System.Drawing.Size(435, 22);
+            this.textBox_Send.TabIndex = 3;
+            // 
+            // button_Send
+            // 
+            this.button_Send.Location = new System.Drawing.Point(488, 519);
+            this.button_Send.Name = "button_Send";
+            this.button_Send.Size = new System.Drawing.Size(75, 23);
+            this.button_Send.TabIndex = 4;
+            this.button_Send.Text = "Send";
+            this.button_Send.UseVisualStyleBackColor = true;
+            this.button_Send.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(587, 559);
+            this.Controls.Add(this.button_Send);
+            this.Controls.Add(this.textBox_Send);
             this.Controls.Add(this.Server_output);
             this.Controls.Add(this.comboBox_server);
             this.Controls.Add(this.button1);
@@ -82,6 +105,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox_server;
         private System.Windows.Forms.TextBox Server_output;
+        private System.Windows.Forms.TextBox textBox_Send;
+        private System.Windows.Forms.Button button_Send;
     }
 }
 
--- DST_Server_GUI/Form1.cs
+++ DST_Server_GUI/Form1.cs
@@ -15,6 +15,8 @@ namespace DST_Server_GUI
     {
         private StreamWriter mStreamWriter;
         private StringBuilder output = new StringBuilder("");
+        Process Server_Master;
+        int line_cnt = 0;
 
         public Form1()
         {
@@ -28,33 +30,54 @@ namespace DST_Server_GUI
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Process Server_Master = new Process();
-            //info.FileName = "dontstarve_dedicated_server_nullrenderer";
-            //info.WorkingDirectory = @"E:\Steam\steamapps\common\Don't Starve Together\bin";
-            //info.Arguments = "-console -cluster DST_Server -shard Master";
-            Server_Master.StartInfo.FileName = "cmd.exe";
-            Server_Master.StartInfo.CreateNoWindow = true;
-            Server_Master.StartInfo.UseShellExecute = false;
-            Server_Master.StartInfo.RedirectStandardOutput = true;
-            Server_Master.StartInfo.RedirectStandardError = true;
-            Server_Master.StartInfo.RedirectStandardInput = true;
-
-            Server_Master.OutputDataReceived += new DataReceivedEventHandler((object sendingProcess, DataReceivedEventArgs outLine) =>
+            try { Process.GetProcessById(Server_Master.Id); }
+            catch  /* If process doesn't exist, create new process. */
             {
-                if (!String.IsNullOrEmpty(outLine.Data)) {
-                    output.Append(Environment.NewLine + outLine.Data);
-                }
-            });
+                Server_Master = new Process();
+                //info.FileName = "dontstarve_dedicated_server_nullrenderer";
+                //info.WorkingDirectory = @"E:\Steam\steamapps\common\Don't Starve Together\bin";
+                //info.Arguments = "-console -cluster DST_Server -shard Master";
+                Server_Master.StartInfo.FileName = "cmd.exe";
+                Server_Master.StartInfo.CreateNoWindow = true;
+                Server_Master.StartInfo.UseShellExecute = false;
+                Server_Master.StartInfo.RedirectStandardOutput = true;
+                Server_Master.StartInfo.RedirectStandardError = true;
+                Server_Master.StartInfo.RedirectStandardInput = true;
 
-            Server_Master.Start();
+                Server_Master.OutputDataReceived += new DataReceivedEventHandler(SortOutputHandler);
 
-            mStreamWriter = Server_Master.StandardInput;
-            Server_Master.BeginOutputReadLine();
+                Server_Master.Start();
+
+                mStreamWriter = Server_Master.StandardInput;
+                line_cnt = 0;
+                Server_Master.BeginOutputReadLine();
+            }
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             
+        }
+
+        private void SortOutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            if (Server_output.InvokeRequired) { Server_output.BeginInvoke(new DataReceivedEventHandler(SortOutputHandler), new[] { sendingProcess, outLine }); }
+            else {
+                if (!String.IsNullOrEmpty(outLine.Data))  {
+                    while (line_cnt >= 30) {
+                        output.Remove(0, output.ToString().IndexOf("\n") + 1);
+                        line_cnt--;
+                    }
+                    output.Append(outLine.Data + Environment.NewLine);
+                    line_cnt++;
+                    Server_output.Text = output.ToString();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mStreamWriter.WriteLine(textBox_Send.Text.ToString());
         }
     }
 }
